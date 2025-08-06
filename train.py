b0fbd51c648878edaa372bf66a6ec361ab275df2
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2025-08-06 18:07:00 +0200

    update seg

--- train.py
+++ train.py
@@ -1,7 +1,8 @@
 from ultralytics import YOLO
+import wandb
 
-# Load a model
-model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
+wandb.init(project="water-level")
+model = YOLO("yolo11n-seg.pt")  # load a pretrained model (recommended for training)
 
 # Train the model with 2 GPUs
 results = model.train(data="water.yaml", epochs=100, imgsz=640, device=[0, 1])
