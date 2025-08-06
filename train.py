643b400f54758137261d43df0d7398f293a10a2e
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2025-08-06 20:45:06 +0200

    update 200 epoches

--- train.py
+++ train.py
@@ -5,4 +5,4 @@ wandb.init(project="water-level")
 model = YOLO("yolo11n-seg.pt")  # load a pretrained model (recommended for training)
 
 # Train the model with 2 GPUs
-results = model.train(data="water.yaml", epochs=100, imgsz=640, device=[0, 1])
+results = model.train(data="water.yaml", epochs=300, imgsz=640, device=[0, 1], name="yolo11n-seg-300ep")
