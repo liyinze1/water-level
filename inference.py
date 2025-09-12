18c6ca1ed93d48024fc43396417ed891f16dc378
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2025-09-12 15:11:37 +0200

    update data seg

--- inference.py
+++ inference.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a pretrained YOLO11n model
-model = YOLO("runs/segment/yolo11n-seg-300ep/weights/best.pt")
+model = YOLO("runs/segment/yolo11l-seg-300ep/weights/best.pt")
 
 # Run inference on 'bus.jpg' with arguments
-model.predict('data.txt', save=True, imgsz=640, conf=0.5)
+model.predict('./Leuven', save=True, imgsz=640, conf=0.5, name='11l-predict')
