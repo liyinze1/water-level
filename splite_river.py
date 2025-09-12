a1e39b5328fb39341fd759d45438b87e1fc4b3c4
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2025-09-12 15:11:37 +0200

    update data seg

--- splite_river.py
+++ splite_river.py
@@ -7,25 +7,37 @@ f.close()
 
 rate = 0.2
 
+rivers = []
+for file in files:
+    if 'ADE20K' not in file:
+        rivers.append(file)
+
+files = rivers
+
+f = open('river.txt', 'w')
+for file in files:
+    f.write(file)
+f.close()
+
 random.shuffle(files)
 train_files = files[:int(len(files) * (1 - rate))]
 val_files = files[int(len(files) * (1 - rate)):]
 
-f = open('water_train.txt', 'w')
+f = open('river_train.txt', 'w')
 for file in train_files:
     f.write(file)
 f.close()
 
-f = open('water_val.txt', 'w')
+f = open('river_val.txt', 'w')
 for file in val_files:
     f.write(file)
 f.close()
 
-f = open('water.yaml', 'w')
+f = open('river.yaml', 'w')
 abs_path = os.path.abspath('.')
 f.write(f'path: {abs_path}\n')
-f.write('train: water_train.txt\n')
-f.write('val: water_val.txt\n')
+f.write('train: river_train.txt\n')
+f.write('val: river_val.txt\n')
 f.write('names:\n')
 f.write('  0: water\n')
 f.close()
