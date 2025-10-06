cf9523270ce4fcc27567b95eff1c194fb0ddd5f6
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2025-10-06 15:24:35 +0200

    update train

--- train.py
+++ train.py
@@ -5,9 +5,9 @@ import os
 os.system('export MKL_THREADING_LAYER=GNU')
 
 wandb.init(project="water-level")
-# model = YOLO("yolo11n-seg.pt")  # load a pretrained model (recommended for training)
 
-model = YOLO("yolo11l-seg.pt") 
+model = YOLO("yolo11n-seg.pt")
+#model = YOLO("yolo11l-seg.pt") 
 
 # Train the model with 2 GPUs
 results = model.train(data="water.yaml", epochs=300, imgsz=640, device=[0, 1], name="yolo11l-seg-300ep")
