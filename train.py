24f2a92286f5a81a3ba8b1fba9522f6ac0b95bc9
h3dema <henrique.duartemoura@imec.be>
2025-10-07 08:48:39 +0000

    check number of GPU's available

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import torch
 from ultralytics import YOLO
 import wandb
 import os
@@ -9,5 +10,6 @@ wandb.init(project="water-level")
 model = YOLO("yolo11n-seg.pt")
 #model = YOLO("yolo11l-seg.pt") 
 
-# Train the model with 2 GPUs
-results = model.train(data="water.yaml", epochs=300, imgsz=640, device=[0, 1], name="yolo11l-seg-300ep")
+# Train the model with 2 GPUs, if available
+devices = [0, 1] if torch.cuda.device_count() > 1 else [0]
+results = model.train(data="water.yaml", epochs=300, imgsz=640, device=devices, name="yolo11l-seg-300ep")
