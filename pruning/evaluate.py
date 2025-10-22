4e4864606d6c87a33ae39e85b35d77f99132d6e3
h3dema <henriquemoura@hotmail.com>
2025-10-22 15:44:49 +0000

    decode yolov11 output

--- pruning/evaluate.py
+++ pruning/evaluate.py
@@ -29,11 +29,38 @@ import numpy as np
 import cv2
 import torch
 import torchvision.transforms.functional as TF
+from ultralytics.utils.tal import make_anchors, dist2bbox, TaskAlignedAssigner
+
+
+
+def bbox_decode(anchor_points: torch.Tensor, pred_dist: torch.Tensor, proj, use_dfl: bool = True) -> torch.Tensor:
+    """Decode predicted object bounding box coordinates from anchor points and distribution."""
+    if use_dfl:
+        b, a, c = pred_dist.shape  # batch, anchors, channels
+        pred_dist = pred_dist.view(b, a, 4, c // 4).softmax(3).matmul(proj.type(pred_dist.dtype))
+        # pred_dist = pred_dist.view(b, a, c // 4, 4).transpose(2,3).softmax(3).matmul(self.proj.type(pred_dist.dtype))
+        # pred_dist = (pred_dist.view(b, a, c // 4, 4).softmax(2) * self.proj.type(pred_dist.dtype).view(1, 1, -1, 1)).sum(2)
+    return dist2bbox(pred_dist, anchor_points, xywh=False)
 
 
 def decode_yolov11_segmentation(outputs, conf_thresh=0.5, image_size=(640, 640)):
-    pred = outputs[0][0]  # shape: (37, 8400)
-    proto = outputs[1][2][0] # shape: (32, 160, 160)
+    """
+    Decodes the output of the YOLO model into bounding boxes and segmentation masks.
+
+    Parameters:
+    outputs (list): The output of the YOLO model, which contains 2 entries.
+    conf_thresh (float, optional): The minimum confidence score for a bounding box to be considered valid. Defaults to 0.5.
+    image_size (tuple, optional): The size of the input image. Defaults to (640, 640).
+
+    Returns:
+    tuple: A tuple containing the bounding boxes and segmentation masks. The bounding boxes are represented as a tensor of shape (N, 4) where N is the number of bounding boxes. The segmentation masks are represented as a tensor of shape (N, H, W) where H and W are the height and width of the input image, respectively.
+    """
+    # YOLO outputs variable is different if model is .train() or .eval()
+    feats, pred_masks, proto = outputs if len(outputs) == 3 else outputs[1]
+    batch_size, _, mask_h, mask_w = proto.shape  # batch size, number of masks, mask height, mask width
+
+    pred = outputs[0][0].detach().cpu()  # shape: (37, 8400)
+    proto = outputs[1][2][0].detach().cpu()  # shape: (32, 160, 160)
 
     # Extract components
     boxes = pred[0:4, :]  # x, y, w, h
@@ -65,6 +92,75 @@ def decode_yolov11_segmentation(outputs, conf_thresh=0.5, image_size=(640, 640))
     masks_resized[masks_resized <= .5] = 0
 
     return boxes_pixel, masks_resized
+
+
+def decode_yolov11_segmentation2(
+    outputs,  # output from yolov11 model's model (i.e. outputs = yolov11.model(inputs))
+    conf_thresh=0.5, 
+    device = "cpu",
+    nc = 1,  # yolo_model.model.nc
+    reg_max = 16,  # see https://docs.ultralytics.com/reference/nn/modules/head/#ultralytics.nn.modules.head.WorldDetect.forward
+    stride = [8, 16, 32],  # model.stride
+    ):
+    """
+    Decodes the output of the YOLO model into bounding boxes and segmentation masks.
+
+    Parameters:
+    outputs (list): The output of the YOLO model, which contains 2 or 3 entries.
+    conf_thresh (float, optional): The minimum confidence score for a bounding box to be considered valid. Defaults to 0.5.
+    image_size (tuple, optional): The size of the input image. Defaults to (640, 640).
+
+    Returns:
+    tuple: A tuple containing the bounding boxes and segmentation masks. The bounding boxes are represented as a tensor of shape (N, 4) where N is the number of bounding boxes. The segmentation masks are represented as a tensor of shape (N, H, W) where H and W are the height and width of the input image, respectively.
+    """
+    # YOLO outputs variable is different if model is .train() or .eval()
+    feats, pred_masks, proto = outputs if len(outputs) == 3 else outputs[1]
+    batch_size, _, mask_h, mask_w = proto.shape  # batch size, number of masks, mask height, mask width
+
+    no = nc + reg_max * 4    
+    pred_distri, pred_scores = torch.cat(
+        [xi.view(feats[0].shape[0], no, -1) for xi in feats], 2).split((reg_max * 4, nc), 1
+    )
+
+    # B, grids, ..
+    pred_scores = pred_scores.permute(0, 2, 1).contiguous()
+    pred_distri = pred_distri.permute(0, 2, 1).contiguous()
+    pred_masks = pred_masks.permute(0, 2, 1).contiguous()
+
+    dtype = pred_scores.dtype
+    image_size = torch.tensor(feats[0].shape[2:], device=device, dtype=dtype) * stride[0]  # image size (h,w)
+    image_size =  [int(x) for x in list(image_size)]
+
+    anchor_points, stride_tensor = make_anchors(feats, stride, 0.5)
+    proj = torch.arange(reg_max, dtype=torch.float, device=device)
+
+    pred_bboxes = bbox_decode(anchor_points, pred_distri, proj, True)  # xyxy, (b, h*w, 4)
+
+    # Filter by confidence
+    keep = (pred_scores > 0.5).squeeze(-1).squeeze(0)  # select only objects with score > 0.5 and return shape (8400,)
+    boxes = pred_bboxes[:, keep].squeeze(0)  # shape: (n, 4)
+
+    # Convert boxes from (x, y, w, h) to (x1, y1, x2, y2)
+    cx, cy, w, h = boxes[0], boxes[1], boxes[2], boxes[3]
+    x1 = (cx - w / 2)
+    y1 = (cy - h / 2)
+    x2 = (cx + w / 2)
+    y2 = (cy + h / 2)
+    boxes_pixel = torch.stack([x1, y1, x2, y2], dim=1).int()
+
+    # Reconstruct masks
+    mask_coeffs = pred_masks[:, keep].squeeze(0).detach().cpu()  # shape: (1, n, 32)
+    pred_mask = torch.einsum('ni,jihw->nhw', mask_coeffs, proto)
+    masks = torch.sigmoid(pred_mask)  # (N, H, W)
+
+    # Resize masks to image size
+    masks_resized = TF.resize(masks.unsqueeze(1), image_size, interpolation=TF.InterpolationMode.BILINEAR).squeeze(1)
+
+    # masks as binary
+    masks_resized[masks_resized > .5] = 1
+    masks_resized[masks_resized <= .5] = 0
+
+    return boxes_pixel, masks_resized.detach().cpu()
 
 
 def combine_masks(masks):
@@ -115,8 +211,12 @@ def resize_polygons(labels, target_size):
 
 
 def create_binary_mask(scaled_labels, imgsz, selected_class_id=0):
+    if isinstance(imgsz, int): 
+        # expand imgsz
+        imgsz = (imgsz, imgsz)  
+
     # Initialize empty mask
-    mask = np.zeros((imgsz, imgsz), dtype=np.uint8)
+    mask = np.zeros(imgsz, dtype=np.uint8)
 
     for class_id, polygon in scaled_labels:
         if class_id != selected_class_id:
