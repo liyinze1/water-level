fix gradient propagation

pruning/evaluate.py
@@ -154,23 +154,24 @@ def decode_yolov11_segmentation2(
     masks = torch.sigmoid(pred_mask)  # (N, H, W)
 
     # Resize masks to image size
+    # NOTE: masks_resized must be a tensor, and keep its gradient
     masks_resized = TF.resize(masks.unsqueeze(1), image_size, interpolation=TF.InterpolationMode.BILINEAR).squeeze(1)
 
     # masks as binary
     masks_resized[masks_resized > .5] = 1
     masks_resized[masks_resized <= .5] = 0
 
-    return boxes_pixel, masks_resized.detach().cpu()
+    return boxes_pixel, masks_resized
 
 
-def combine_masks(masks):
-    mask = np.zeros_like(masks[0]).astype(bool)
-    for i in range(masks.shape[0]):
-        block_mask = masks[0].numpy()
-        block_mask = block_mask.astype(bool)
-        # Create a color layer
-        mask = np.logical_or.reduce([mask, block_mask])
-    return mask
+def combine_masks(masks: torch.Tensor) -> torch.Tensor:
+    combined_mask = masks[0].float()
+    for i in range(1, masks.shape[0]):
+        combined_mask = combined_mask + masks[i].float()
+    
+    # need to clamp it to keep it binary
+    combined_mask = combined_mask.clamp(0, 1)
+    return combined_mask
 
 
 def decode_raw_labels(lines):

pruning/taylor_pruning.py
@@ -31,7 +31,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "../scaling"))
 import yaml
 
 import torch
-import torch.nn as nn
+from torch import nn
 # import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from torchvision.transforms import v2
@@ -106,15 +106,14 @@ if __name__ == '__main__':
         print("No masks found")
         exit(0)
     
-    pred_mask = combine_masks(masks)
-    pred_mask = torch.tensor(pred_mask.astype(float))  # needs to be float tensor
     # decode input into a mask
+    pred_mask = combine_masks(masks)
     target_mask = create_binary_mask(targets[0], img_shape)
     target_mask = torch.tensor(target_mask.astype(float), requires_grad=True)  # needs to be float tensor with gradient enabled
 
+    print("Getting gradients")
     loss = criterion(pred_mask, target_mask)
     print(f"Loss value: {loss.item()}")
-    print("Getting gradients")
     model.zero_grad()
     loss.backward()
     # for name, param in model.named_parameters():